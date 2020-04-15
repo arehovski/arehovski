b949584f9f566ca47e5fc86cc05f640d4bdc5d02
arehovski <sergei_arehovski@mail.ru>
2020-04-15 21:45:49 +0300

    Added comments into 'quad_equation'

--- stormnet_math/equations.py
+++ stormnet_math/equations.py
@@ -1,4 +1,13 @@
-def quad_ecuation(a, b, c):
+def quad_equation(a, b, c):
+    """
+    Returns the roots of quadratic equation or none if discriminant less than 0.
+    quad_equation(4, 4, 1)
+    (-0.5, -0.5)
+    quad_equation(4, 4, 2)
+    None
+    quad_equation(4, 8, 2)
+    (-0.2929, -1.7071)
+    """
     d = (b ** 2) - (4 * a * c)
     if d < 0:
         return None
@@ -6,4 +15,3 @@ def quad_ecuation(a, b, c):
         x_1 = (- b + d ** 0.5) / (2 * a)
         x_2 = (- b - d ** 0.5) / (2 * a)
         return round(x_1, 4), round(x_2, 4)
-
