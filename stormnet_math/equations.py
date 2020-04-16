d21e9da4ebc5b66ab68ce331da719be1b33963be
arehovski <sergei_arehovski@mail.ru>
2020-04-16 20:29:51 +0300

    Added doctest

--- stormnet_math/equations.py
+++ stormnet_math/equations.py
@@ -1,11 +1,10 @@
 def quad_equation(a, b, c):
     """
     Returns the roots of quadratic equation or none if discriminant less than 0.
-    quad_equation(4, 4, 1)
+    >>> quad_equation(4, 4, 1)
     (-0.5, -0.5)
-    quad_equation(4, 4, 2)
-    None
-    quad_equation(4, 8, 2)
+    >>> quad_equation(4, 4, 2)
+    >>> quad_equation(4, 8, 2)
     (-0.2929, -1.7071)
     """
     d = (b ** 2) - (4 * a * c)
@@ -15,3 +14,7 @@ def quad_equation(a, b, c):
         x_1 = (- b + d ** 0.5) / (2 * a)
         x_2 = (- b - d ** 0.5) / (2 * a)
         return round(x_1, 4), round(x_2, 4)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
